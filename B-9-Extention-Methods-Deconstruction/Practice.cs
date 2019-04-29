1df1627b63dc121a8a06db49ac8db4bb515e1250
massanastya@gmail.com <massanastya@gmail.com>
2019-04-29 10:41:26 +0300

    1

--- B-9-Extention-Methods-Deconstruction/Practice.cs
+++ B-9-Extention-Methods-Deconstruction/Practice.cs
@@ -15,9 +15,33 @@ namespace Base_Lesson_9.Practice
         /// Вывести на консоль длину вектора по координатам.
         /// </summary>
         public static void L9_P_EX_1_from_2()
-        {
+		{
+			Coordinates coord = new Coordinates(11, 12, 33);
+			var (X, Y, Z, vector) = coord;
+			Console.WriteLine(vector);
+			Console.ReadKey();
+		}
+		public struct Coordinates
+		{
+			private int x, y, z;
 
-        }
+			public Coordinates(int x, int y, int z)
+			{
+				this.x = x;
+				this.y = y;
+				this.z = z;
+			}
+
+			public void Deconstruct(out int X, out int Y, out int Z, out double vector)
+			{
+				X = x;
+				Y = y;
+				Z = z;
+				vector =Math.Round(Math.Sqrt(X*X+Y*Y+Z*Z),2);
+			}
+		}
+
+	
 
         /// <summary>
         /// L9-P-EX-2/2. 
@@ -28,7 +52,22 @@ namespace Base_Lesson_9.Practice
         /// </summary>
         public static void L9_P_EX_2_from_2()
         {
-
+	        currentHour
+			DateTime date = new DateTime();
+	        var (_, _, _, currentHour, currentMinute, _) = date;
+	        Console.WriteLine(currentHour + " " + currentMinute);
+		}
+        public static class ExtentionDateTime
+        {
+	        public static void Deconstruct(out int year, out int month, out int day, out int hour, out int minute, out int second)
+	        {
+		        year = DateTime.Now.Year;
+		        month = DateTime.Now.Month;
+		        day = DateTime.Now.Day;
+		        hour = DateTime.Now.Hour;
+		        minute = DateTime.Now.Minute;
+		        second = DateTime.Now.Second;
+	        }
         }
-    }
+	}
 }
